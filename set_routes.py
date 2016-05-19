79c618501287efd3a6ff94884e254591ba112ef4
Cornelius Keller <cke@marketlogicsoftware.com>
2016-05-19 23:12:20 +0200

    hetzner subnet routes configuration take 2

--- set_routes.py
+++ set_routes.py
@@ -13,5 +13,6 @@ gws = netifaces.gateways()
 default_gw = gws['default'][netifaces.AF_INET]
 
 for ip in network:
-    if ip != network.network and ip != network.broadcast:
-        subprocess.call(["route", "add", "-host", str(ip),  "gw", default_gw[0]])
+    if ip != network.network and ip != network.broadcast and ip != netaddr.IPAddress(default_gw[0]):
+#        subprocess.call(["route", "add", "-host", str(ip),  "gw", default_gw[0]])
+         print ip
