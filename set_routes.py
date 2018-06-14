523c303762e440657b9fe8f00cb0e963738c8602
Cornelius Keller <cke@marketlogicsoftware.com>
2018-06-14 22:24:25 +0200

    sleep infinit after setting routes; so we can use this in a k8s daemonset

--- set_routes.py
+++ set_routes.py
@@ -3,6 +3,7 @@ import netifaces
 import netaddr
 import subprocess
 import os
+from threading import Event
 
 for iface in netifaces.interfaces():
     if iface.startswith("en"):
@@ -20,3 +21,5 @@ default_gw = gws['default'][netifaces.AF_INET]
 for ip in network:
     if ip != network.network and ip != network.broadcast and ip != netaddr.IPAddress(default_gw[0]):
         subprocess.call(["route", "add", "-host", str(ip),  "gw", default_gw[0]])
+
+Event.wait()
