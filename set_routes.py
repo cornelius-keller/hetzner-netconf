c059f2490663416de4a5db30a201342cd5b8df55
Cornelius Keller <cke@marketlogicsoftware.com>
2018-06-14 23:12:40 +0200

    check if route is not already there before adding

--- set_routes.py
+++ set_routes.py
@@ -13,6 +13,13 @@ for iface in netifaces.interfaces():
             if ifconf['netmask'] != '255.255.255.255':
                break
 
+# get current route destinations
+routes = subprocess.check_output("route")
+destinations = []
+
+for line in routes.split("\n"):
+    if re.match("^\d+\.\d+\.\d+\.\d+.*", line):
+        destinations.append(line.split()[0])
 
 network = netaddr.IPNetwork("%s/%s" % (ifconf['addr'], ifconf['netmask']))
 
@@ -21,7 +28,7 @@ gws = netifaces.gateways()
 default_gw = gws['default'][netifaces.AF_INET]
 
 for ip in network:
-    if ip != network.network and ip != network.broadcast and ip != netaddr.IPAddress(default_gw[0]):
+    if ip != network.network and ip != network.broadcast and ip != netaddr.IPAddress(default_gw[0]) and str(ip) not in destinations:
         subprocess.call(["route", "add", "-host", str(ip),  "gw", default_gw[0]])
 
 Event.wait()
